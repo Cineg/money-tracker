e57bc508eaa51ef692becf8624cb281d9a331a10
cineg <cineg7@gmail.com>
2018-08-03 11:43:53 +0200

    init vue app

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  props: {
+    msg: String
+  },
+
+  computed: {
+    monthNow: function() {
+      const date = new Date();
+      return date.getMonth() + 1;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
